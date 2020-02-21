0652a8f80cef4b84e403575760a6b6dc561db08e
pierremont <pierre_montaigne@yahoo.com>
2020-02-21 15:37:39 +0200

    new

--- write_file.py
+++ write_file.py
@@ -1,9 +1,10 @@
 #! /bin/python3.6
 
 from datetime import datetime
+import os
 
 f = open("testfile.txt","a+")
 
 f.write("am primit 200 ok in " + str(datetime.now()) + "\r\n")
-f.write($my_param + "\r\n")
+f.write(os.environ['my_param'] + "\r\n")
 f.close()
