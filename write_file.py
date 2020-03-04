38390c5a221b8164a1d653b9cc05063a45d79981
pierremont <pierre_montaigne@yahoo.com>
2020-03-04 13:07:38 +0200

    v2

--- write_file.py
+++ write_file.py
@@ -3,8 +3,9 @@
 from datetime import datetime
 import os
 
+os.chdir("/home/petrus/python/github/python_tests")
 f = open("testfile.txt","a+")
 
 f.write("am primit 200 ok in " + str(datetime.now()) + "\r\n")
-f.write(os.environ['my_param'] + "\r\n")
+#f.write(os.environ['my_param'] + "\r\n")
 f.close()
